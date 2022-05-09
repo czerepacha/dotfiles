64ff52dad7056c3fbc18dfb97dc2e55af67677cc
lpsk <lipski@fastmail.com>
2022-05-09 08:59:40 +0200

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -124,7 +124,7 @@ require('telescope').setup {
 require('telescope').load_extension 'fzf'
 
 --Add leader shortcuts
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers)
+vim.keymap.set('n', '<leader><space>', require('telescope.builtin').builtin)
 vim.keymap.set('n', '<leader>sf', function()
   require('telescope.builtin').find_files { previewer = false }
 end)
@@ -132,7 +132,7 @@ vim.keymap.set('n', '<leader>sb', require('telescope.builtin').current_buffer_fu
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags)
 vim.keymap.set('n', '<leader>st', require('telescope.builtin').tags)
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').grep_string)
-vim.keymap.set('n', '<leader>sp', require('telescope.builtin').live_grep)
+vim.keymap.set('n', '<leader>ss', require('telescope.builtin').live_grep)
 vim.keymap.set('n', '<leader>so', function()
   require('telescope.builtin').tags { only_current_buffer = true }
 end)
