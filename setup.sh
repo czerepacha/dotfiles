a3f966dc805427f44a975c94ce9397784f320fce
lpsk <lipski@fastmail.com>
2022-08-12 00:48:10 +0200

    update

--- setup.sh
+++ setup.sh
@@ -13,11 +13,10 @@ sudo apt install /tmp/nvim-linux64.deb
 curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain stable
 
 # install go
+GOVERSION=go1.19.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go
-if ! echo "956f8507b302ab0bb747613695cdae10af99bbd39a90cae522b7c0302cc27245 /tmp/go1.18.3.linux-amd64.tar.gz" | sha256sum -c; then
-  wget -O /tmp/go1.18.3.linux-amd64.tar.gz https://go.dev/dl/go1.18.3.linux-amd64.tar.gz
-fi
-sudo tar -C /usr/local -xzf /tmp/go1.18.3.linux-amd64.tar.gz
+sudo wget -O /tmp/$GOVERSION https://go.dev/dl/$GOVERSION
+sudo tar -C /usr/local -xzf /tmp/$GOVERSION
 sudo chmod -R a+rx /usr/local/go
 
 # install oh-my-zsh
