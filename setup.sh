f9a597c0a7a9ab8aecfc787ffdfb4a4a9c8d9a17
lpsk <lipski@fastmail.com>
2022-08-12 10:45:47 +0200

    update

--- setup.sh
+++ setup.sh
@@ -4,19 +4,17 @@
 sudo apt-get install -y zsh curl git
 
 # install neovim
-if ! echo "dce77cae95c2c115e43159169e2d2faaf93bce6862d5adad7262f3aa3cf60df8 /tmp/nvim-linux64.deb" | sha256sum -c; then
-  wget -O /tmp/nvim-linux64.deb https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb
-fi
+sudo wget -O /tmp/nvim-linux64.deb https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb
 sudo apt install /tmp/nvim-linux64.deb
 
 # install rust
 curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain stable
 
 # install go
-GOVERSION=go1.19.linux-amd64.tar.gz
+GORELEASE=go1.19.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go
-sudo wget -O /tmp/$GOVERSION https://go.dev/dl/$GOVERSION
-sudo tar -C /usr/local -xzf /tmp/$GOVERSION
+sudo wget -O /tmp/$GORELEASE https://go.dev/dl/$GORELEASE
+sudo tar -C /usr/local -xzf /tmp/$GORELEASE
 sudo chmod -R a+rx /usr/local/go
 
 # install oh-my-zsh
