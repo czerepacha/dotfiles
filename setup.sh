1fc9ddc6ef19b7c65de9d1264047751790d7262e
lpsk <lipski@fastmail.com>
2022-08-17 20:06:54 +0200

    update

--- setup.sh
+++ setup.sh
@@ -1,20 +1,29 @@
 #!/bin/sh
 
+# versions
+NVIM_RELEASE="v0.7.2"
+GO_VERSION="1.19"
+RUST_VERSION="stable"
+
 # install packages
 sudo apt-get install -y zsh curl git
 
+# install kubectl
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo chmod 755 ./kubectl && sudo mv ./kubectl /usr/local/bin/
+
 # install neovim
-sudo wget -O /tmp/nvim-linux64.deb https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb
+sudo wget -O /tmp/nvim-linux64.deb "https://github.com/neovim/neovim/releases/download/${NVIM_RELEASE}/nvim-linux64.deb"
 sudo apt install /tmp/nvim-linux64.deb
 
 # install rust
-curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain stable
+curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain "${RUST_VERSION}"
 
 # install go
-GORELEASE=go1.19.linux-amd64.tar.gz
+GORELEASE="go${GO_VERSION}.linux-amd64.tar.gz"
 sudo rm -rf /usr/local/go
-sudo wget -O /tmp/$GORELEASE https://go.dev/dl/$GORELEASE
-sudo tar -C /usr/local -xzf /tmp/$GORELEASE
+sudo wget -O "/tmp/${GORELEASE}" "https://go.dev/dl/${GORELEASE}"
+sudo tar -C /usr/local -xzf "/tmp/${GORELEASE}"
 sudo chmod -R a+rx /usr/local/go
 
 # install oh-my-zsh
