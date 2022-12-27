248c39d824cd1ed4b7fb53d9e379acca18ab4568
Jakub Lipski <jakub.lipski@aplitt.pl>
2022-12-27 11:50:35 +0100

    update

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+set -e -v
+
 # versions
 NVIM_RELEASE="v0.8.1"
 GO_VERSION="1.19"
 RUST_VERSION="stable"
 
 # install packages
-sudo apt-get -qq install -y zsh curl git make python3 python3-pip
+sudo apt-get -qq install -y zsh curl git make python3 python3-pip software-properties-common
 
 # install kubectl
 sudo curl -sL "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" -o /tmp/kubectl
@@ -17,7 +19,8 @@ sudo wget -qO /tmp/nvim-linux64.deb "https://github.com/neovim/neovim/releases/d
 sudo apt-get -qq install /tmp/nvim-linux64.deb
 
 # install rust
-curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain "${RUST_VERSION}"
+wget -qO /tmp/rustup-init "https://static.rust-lang.org/rustup/dist/x86_64-unknown-linux-gnu/rustup-init"
+chmod +x /tmp/rustup-init && /tmp/rustup-init -y --quiet --default-toolchain stable > /dev/null 2>&1
 
 # install go
 GORELEASE="go${GO_VERSION}.linux-amd64.tar.gz"
@@ -26,34 +29,37 @@ sudo wget -qO "/tmp/${GORELEASE}" "https://go.dev/dl/${GORELEASE}"
 sudo tar -C /usr/local -xzf "/tmp/${GORELEASE}"
 sudo chmod -R a+rx /usr/local/go
 
-# install nvm
-curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-nvm install --lts
-nvm use --lts
+# install nodejs
+DISTRO="$(lsb_release -s -c)"
+VERSION=node_18.x
+KEYRING=/usr/share/keyrings/nodesource.gpg
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | gpg --dearmor | sudo tee "$KEYRING" > /dev/null 2>&1
+echo "deb https://deb.nodesource.com/node_18.x kinetic main" | sudo tee /etc/apt/sources.list.d/nodesource.list > /dev/null 2>&1
+echo "deb-src https://deb.nodesource.com/node_18.x kinetic main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list> /dev/null 2>&1
+sudo apt-get -qq update && sudo apt-get -qq install nodejs > /dev/null 2>&1
 
 # install oh-my-zsh
 rm -rf ~/.oh-my-zsh
-CHSH=no RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+CHSH=no RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" > /dev/null 2>&1
 
 # install lunarvim
 rm -rf ~/.config/nvim
 mkdir -p ~/.config/nvim
-(cd /tmp && curl -LO https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
-bash /tmp/install.sh -y
+(cd /tmp && curl -sLO https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+bash /tmp/install.sh -y > /dev/null 2>&1
 
 # install fzf
 rm -rf ~/.fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --all
+git clone -q --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all > /dev/null 2>&1
 
 # install ripgrep
-(cd /tmp && curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb)
-sudo dpkg -i /tmp/ripgrep_13.0.0_amd64.deb
+(cd /tmp && curl -sLO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb)
+sudo dpkg -i /tmp/ripgrep_13.0.0_amd64.deb > /dev/null 2>&1
 
 # install bat
-(cd /tmp && curl -LO https://github.com/sharkdp/bat/releases/download/v0.22.1/bat_0.22.1_amd64.deb)
-sudo dpkg -i /tmp/bat_0.22.1_amd64.deb
+(cd /tmp && curl -sLO https://github.com/sharkdp/bat/releases/download/v0.22.1/bat_0.22.1_amd64.deb)
+sudo dpkg -i /tmp/bat_0.22.1_amd64.deb > /dev/null 2>&1
 
 # set up dotfiles
 touch ~/.secrets.sh
